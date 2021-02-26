ac1a220fe77479a6f881b702a0853442376807f6
shubhambhusari <shubhambhusari2@gmail.com>
2021-02-26 13:48:14 +0530

    Added:creating tictactoe board

---
+++ Tictactoe.java
@@ -0,0 +1,18 @@
+	public class Tictactoe {
+		
+	public static void main (String[] args){
+
+		char [] board = createBoard();
+}
+
+	private static char [] createBoard() {
+		char [] board = new char[10];
+
+		for (int i = 0;i < 9; i++){
+
+		board[i] = ' ';
+		} 
+		return board;
+
+}
+}
