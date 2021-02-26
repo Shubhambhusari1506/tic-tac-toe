b393bd4f76a74e1cd2432f6e54decc6c9f0ba11e
shubhambhusari <shubhambhusari2@gmail.com>
2021-02-27 00:12:36 +0530

    Added: player to choose letter

--- Tictactoe.java
+++ Tictactoe.java
@@ -1,18 +1,34 @@
 public class Tictactoe {
-		
-	public static void main (String[] args){
 
-		char [] board = createBoard();
-}
 
-	private static char [] createBoard() {
-		char [] board = new char[10];
+      char [] board = createBoard();
 
-		for (int i = 0;i < 9; i++){
 
-		board[i] = ' ';
-		} 
-		return board;
+   public  char [] createBoard() {
+      char [] board = new char[10];
 
-}
-}
+      for (int i = 0;i < 9; i++){
+
+      board[i] = ' ';
+      }
+      return board;
+
+   }
+
+
+   public static char selectLetter(Scanner userInput){
+   System.out.println ("Select your letter X or O:   ");
+   return userInput.next().toUpperCase().charAt(0);
+   }
+   public static void main (String[] args) {
+	   
+	   
+	   Scanner userInput = new Scanner(System.in);
+	   char userletter = selectLetter(userInput);
+	   char computerletter = (userletter == 'X') ? 'O':'X';
+	   System.out.println("User letter:"  +userletter);
+	   System.out.println("computer Letter:  " +computerletter);
+	   		Tictactoe tictactoe = new Tictactoe();
+	   		tictactoe.createBoard();
+   }}
+
