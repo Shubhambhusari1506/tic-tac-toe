4c5561a48018c05f6d14eb28e936d1bfcdcf7140
shubhambhusari <shubhambhusari2@gmail.com>
2021-03-04 14:39:38 +0530

    Added: ability for user to make a move

--- Tictactoe.java
+++ Tictactoe.java
@@ -1,71 +1,65 @@
 import java.util.*;
+
 public class Tictactoe {
 
+	static Scanner userInput = new Scanner(System.in);
+	static char userLetter;
+	static char computerLetter;
+	static char[] board = new char[10];
 
-      static char [] board = createBoard();
+	public char[] createBoard() {
 
 
-	public  static  char [] createBoard() {
-
-
-      char [] board = new char[10];
-
-      for (int i = 0;i < 9; i++){
-
-      board[i] = ' ';
-      
+	for (  int i=1 ; i < 10; i++ ) {
+			board[i] = ' ';
 		}
-      
 		return board;
+	}
 
-   }
+	public static void chooseLetter() {
 
+	System.out.print("Choose your Letter X or O : ");
 
-   public static char selectLetter(Scanner userInput){
-   
-		System.out.println ("Select your letter X or O:   ");
-   	
-		return userInput.next().toUpperCase().charAt(0);
-   }
+	userLetter = userInput.next().toUpperCase().charAt(0);
 
+		if (userLetter == 'X' || userLetter == 'x' || userLetter == 'o' || userLetter == 'O') {
+			computerLetter = (userLetter == 'X') ? 'O' : 'X';
+		} else {
+			System.out.println("Invalid INPUT");
+			chooseLetter();
+		}
 
-   static  void displayBoard(){
+	}
+	public static void showBoard(){
 
-	   System.out.println( "    " +board[1]+ "   | "+ board[2]+ "  | " + board[3]+ "     ");
-   	
+		System.out.println( "    " +board[1]+ "   | "+ board[2]+ "  | " + board[3]+ "     ");
 		System.out.println(" ------------------------ ");
-   	
 		System.out.println( "    " +board[4]+ "   | "+ board[5]+ "  | " + board[6]+ "     ");
-   	
 		System.out.println(" ------------------------ ");
-   	
 		System.out.println( "    " +board[7]+ "   | "+ board[8]+ "  | " + board[9]+ "     ");
-   }
+	}
 
-   public static void main (String[] args) {
+	public static void makeMove() {
 
-
-      Scanner userInput = new Scanner(System.in);
-
-      char userletter = selectLetter(userInput);
-
-      char computerletter = (userletter == 'X') ? 'O':'X';
-
-      System.out.println("User letter:"  +userletter);
-
-      System.out.println("computer Letter:  " +computerletter);
-            
-		Tictactoe tictactoe = new Tictactoe();
-      
-      tictactoe.createBoard();
+		System.out.print("Select location between 1-9 : ");
+		int location = userInput.nextInt();
+		if (board[location] != ' ') {
+			System.out.println("Position Already Occupied");
+		}
+	}
 
 
-		displayBoard();
-   }
+	public static void main(String[] args){
+
+	System.out.println("Welcome to TicTacToeGame");
+
+	Tictactoe ticTacToeGame = new Tictactoe();
+
+
+	ticTacToeGame.createBoard();
+		chooseLetter();
+      System.out.println("Computer Letter: " +computerLetter);
+		showBoard();
+		makeMove();
+	}
 }
-
-
-
-
-
-
