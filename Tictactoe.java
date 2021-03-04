4f5f2c0bc3329463b2dfebdff6a45cc4565fa67e
shubhambhusari <shubhambhusari2@gmail.com>
2021-03-04 14:19:36 +0530

    Added: to Show Board

--- Tictactoe.java
+++ Tictactoe.java
@@ -1,34 +1,71 @@
+import java.util.*;
 public class Tictactoe {
 
 
-      char [] board = createBoard();
+      static char [] board = createBoard();
 
 
-   public  char [] createBoard() {
+	public  static  char [] createBoard() {
+
+
       char [] board = new char[10];
 
       for (int i = 0;i < 9; i++){
 
       board[i] = ' ';
-      }
-      return board;
+      
+		}
+      
+		return board;
 
    }
 
 
    public static char selectLetter(Scanner userInput){
-   System.out.println ("Select your letter X or O:   ");
-   return userInput.next().toUpperCase().charAt(0);
+   
+		System.out.println ("Select your letter X or O:   ");
+   	
+		return userInput.next().toUpperCase().charAt(0);
    }
+
+
+   static  void displayBoard(){
+
+	   System.out.println( "    " +board[1]+ "   | "+ board[2]+ "  | " + board[3]+ "     ");
+   	
+		System.out.println(" ------------------------ ");
+   	
+		System.out.println( "    " +board[4]+ "   | "+ board[5]+ "  | " + board[6]+ "     ");
+   	
+		System.out.println(" ------------------------ ");
+   	
+		System.out.println( "    " +board[7]+ "   | "+ board[8]+ "  | " + board[9]+ "     ");
+   }
+
    public static void main (String[] args) {
-	   
-	   
-	   Scanner userInput = new Scanner(System.in);
-	   char userletter = selectLetter(userInput);
-	   char computerletter = (userletter == 'X') ? 'O':'X';
-	   System.out.println("User letter:"  +userletter);
-	   System.out.println("computer Letter:  " +computerletter);
-	   		Tictactoe tictactoe = new Tictactoe();
-	   		tictactoe.createBoard();
-   }}
+
+
+      Scanner userInput = new Scanner(System.in);
+
+      char userletter = selectLetter(userInput);
+
+      char computerletter = (userletter == 'X') ? 'O':'X';
+
+      System.out.println("User letter:"  +userletter);
+
+      System.out.println("computer Letter:  " +computerletter);
+            
+		Tictactoe tictactoe = new Tictactoe();
+      
+      tictactoe.createBoard();
+
+
+		displayBoard();
+   }
+}
+
+
+
+
+
 
